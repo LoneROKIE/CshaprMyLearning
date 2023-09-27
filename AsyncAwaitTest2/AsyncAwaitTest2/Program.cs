0ea19951d5bfb541f6da5757e08bbef8f1032d06
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-09-26 22:12:12 -0600

    add a new example of async await

---
+++ AsyncAwaitTest2/AsyncAwaitTest2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Ejemplo async y await
+
+// creamos la clase principal
+using System.Text;
+
+public class Principal
+{
+    // metodo main
+    public static async Task Main(string[] args)
+    {
+        await MetodoAsincrono1("Encender el sarten");
+        await MetodoAsincrono2("Cortar los platanos");
+        await MetodoAsincrono3("Poner aceite al sarten");
+        await MetodoAsincrono4("Poner los platanos en sarten con aceite");
+    }
+
+    public static async Task MetodoAsincrono1(string tarea)
+    {
+       await Task.Delay(2000);
+       Console.WriteLine("Tarea: "+tarea+", completada");
+    }
+
+    public static async Task MetodoAsincrono2(string tarea)
+    {
+        await Task.Delay(2500);
+        Console.WriteLine("Tarea: " + tarea + ", completada");
+    }
+
+    public static async Task MetodoAsincrono3(string tarea)
+    {
+        await Task.Delay(3000);
+        Console.WriteLine("Tarea: " + tarea + ", completada");
+    }
+    public static async Task MetodoAsincrono4(string tarea)
+    {
+        await Task.Delay(4000);
+        Console.WriteLine("Tarea: " + tarea + ", completada");
+    }
+
+}
