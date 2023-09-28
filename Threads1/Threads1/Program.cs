550d9b8c0982412eb0238a5d72bff03b089ef59a
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-09-27 20:13:29 -0600

    Threads added

---
+++ Threads1/Threads1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Thread t = new Thread(new ThreadStart(MetodoThread));
+        t.Start(); 
+        for(int i = 0; i<5; i++)
+        {
+            Console.WriteLine("Hilo del Main {0}",i);
+            Thread.Sleep(1000);
+        }
+        t.Join();
+        Console.ReadLine();
+    }
+
+    public static void MetodoThread()
+    {
+        for(int i = 0; i<10; i++)
+        {
+            Console.WriteLine("Hilo del metodo: {0}",i);
+            Thread.Sleep(1000);
+        }
+    }
+}
