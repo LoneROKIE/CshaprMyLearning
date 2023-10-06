9150d3fc9967f25e4244c66c38613fbf121fbe5c
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-10-05 22:10:13 -0600

    adde little dicc practice
    it was for showing to my friend

---
+++ DictionaryHashTable/DictionaryHashTable/Program.cs
@@ -0,0 +1,18 @@
+﻿class Principal
+{
+    public static void Main(string[] args)
+    {
+        // dictionary
+        // var d = new Dictionary<key, value>();
+        var d = new Dictionary<string, int>();
+
+        // agregamos un valor al diccionario
+        d.Add("One", 1);
+        d["Two"] = 2; // se aniade porque no existe en el dicc.
+        d["Two"] = 22; // se actualiza porque existe en el dicc.
+        d["Three"] = 3;
+
+        Console.WriteLine(d["Two"]);
+        Console.WriteLine(d["Three"]);
+    }
+}
