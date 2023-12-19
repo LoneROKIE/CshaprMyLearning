0b23b24316f7faee962aa1f47c0ef74142df9d05
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-12-18 21:47:22 -0800

    delegados practica
    practica de los delegados porque no los tenia muy claro

---
+++ DelegatesPractice/DelegatesPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+public class Program
+{
+    public delegate  void MiDelegado(string msj);
+   
+    public static void Main(string[] args)
+    {
+        MiDelegado delegado = new MiDelegado(imprimir);
+        HacerAlgo(delegado, "Hola este es un delegado");
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        HacerAlgo(imprimir,"ConsoleColor ahora es red");
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        HacerAlgo(imprimir, "ConsoleColor ahora es green");
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        HacerAlgo(imprimir, "ConsoleColor ahora es blue");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        HacerAlgo(imprimir, "ConsoleColor ahora es yellow");
+
+        Console.ReadKey();
+    }
+
+    public static void HacerAlgo(MiDelegado delegado,string mensaje)
+    {
+        delegado(mensaje);
+    }
+
+    public static void imprimir(string msj)
+    {
+        Console.WriteLine(msj);
+    }
+
+}
+
